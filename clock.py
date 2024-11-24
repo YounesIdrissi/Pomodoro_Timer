8198ea02cee2f74ba6b3044b8ffdac05f7dfeef1
Younes Idrissi <younesemail0@gmail.com>
2024-11-23 19:20:12 -0500

    third commit

--- clock.py
+++ clock.py
@@ -2,7 +2,7 @@ import time
 
 ticking = False #by default the clock is paused
 
-last_time = [1500]#append time to this list and call the last index (last time)
+last_time = {'Last': 1500}#update last time to this dictionary and call it within start_stop function
 
 def start_stop(s, t):#arguments are time in seconds and ticking
     if t == False:#we switch back and forth between on or off (when start_stop is invoked)
@@ -16,13 +16,21 @@ def start_stop(s, t):#arguments are time in seconds and ticking
             minutes = int(s / 60) % 60
             hours = int((s / 60) / 60)
             print(f"{hours:02}:{minutes:02}:{seconds:02}")
-            last_time.append(s)
+            last_time.update(Last=s)
             s -= 1
             time.sleep(1)
     if t == True:
         return clock(s)
     else:
-        return 
-        #return the last value appended to last_time and display it
+        return #return the last value updated to last_time and display it
 
-start_stop(last_time[-1], ticking)#argument is number of seconds, we want to return the argument inside of the loop when this function is invoked
+start_stop(last_time['Last'], ticking)#argument is number of seconds and ticking, this function is to be button-clicked in GUI
+
+def reset():#resets timer to default
+    pass
+
+def save():#saves session info (we may need a separate system that adds the total work and rest periods)
+    pass
+
+def switch():#switches from work to rest
+    pass
