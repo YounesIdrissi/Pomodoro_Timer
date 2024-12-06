12030cc5392473a6a4523fc9b0952d8785165976
Younes Idrissi <younesemail0@gmail.com>
2024-12-05 21:20:17 -0500

    slight GUI improvements

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ class Pomodoro:
         text = tk.Label(timer, text=f"{hours:02}:{minutes:02}:{seconds:02}", font=("Ariel", 60))
         text.grid(row=0, column=0)
 
-    def instant_switch(self):
+    def instant_switch(self):#spaghetti code...
         #maybe alarm or notification, depends on user testing
         for widget in wr_display.winfo_children():
             widget.destroy()
@@ -147,7 +147,6 @@ class Pomodoro:
         save.grid(row=0, column=2)
 
         save_menu.pack()
-
         
 
     def appender(self, entry, top):
@@ -250,18 +249,21 @@ if __name__ == '__main__':
     root.geometry("700x700")
     root.title("Pomodoro")
 
-    timer = tk.Frame(root)
+    two_sides = tk.Frame(root)#GUI split into two sides: options and timer
+    two_sides.columnconfigure(0, weight=1)
+
+    timer = tk.Frame(two_sides)#right side
     timer.columnconfigure(0, weight=1)
+
+    side_panel = tk.Frame(two_sides)#left side
+    side_panel.columnconfigure(0, weight=1)
 
     session = tk.Frame(root)
     session.columnconfigure(0, weight=1)#general info
     session.columnconfigure(1, weight=1)#delete session
 
-    wr_display = tk.Frame(root)
+    wr_display = tk.Frame(root)#part of root
     wr_display.columnconfigure(0, weight=1)
-
-    settings = tk.Frame(root)
-    settings.columnconfigure(0, weight=1)
     
     clock_thread = threading.Thread(target=pomodoro.clock)#this is a CHILD/WORKER THREAD, which can execute its target function 
                                                 #simultaneously while we do our other processes
@@ -271,28 +273,34 @@ if __name__ == '__main__':
     ssbtn.config(command=pomodoro.start_stop)
     ssbtn.grid(row=1, column=0)
 
-    rbtn = tk.Button(timer, text="Reset", font=("Ariel", 20))
+    #below text and buttons are part of the side panel
+    options = tk.Label(side_panel, text="Options", font=("Ariel", 30))
+    options.grid(row=0, column=0)
+
+    rbtn = tk.Button(side_panel, text="Reset", font=("Ariel", 20))
     rbtn.config(command=pomodoro.reset)
-    rbtn.grid(row=2, column=0)
+    rbtn.grid(row=1, column=0)
 
-    sbtn = tk.Button(timer, text="Save", font=("Ariel", 20))
+    sbtn = tk.Button(side_panel, text="Save", font=("Ariel", 20))
     sbtn.config(command=pomodoro.save)
-    sbtn.grid(row=3, column=0)
+    sbtn.grid(row=2, column=0)
 
-    switchbtn = tk.Button(timer, text="Switch", font=("Ariel", 20))
+    switchbtn = tk.Button(side_panel, text="Switch", font=("Ariel", 20))
     switchbtn.config(command=pomodoro.instant_switch)
-    switchbtn.grid(row=4, column=0)
+    switchbtn.grid(row=3, column=0)
 
-    settingsbtn = tk.Button(timer, text="Settings", font=("Ariel", 20))
+    settingsbtn = tk.Button(side_panel, text="Settings", font=("Ariel", 20))
     settingsbtn.config(command=pomodoro.settings_gate)
-    settingsbtn.grid(row=5, column=0)
-
-    settings.pack()
+    settingsbtn.grid(row=4, column=0)
 
     wr_display.pack()
 
-    session.pack()
+    side_panel.grid(row=0, column=0)#left side
 
-    timer.pack()
+    timer.grid(row=0, column=1)#right side
+
+    two_sides.pack()
+
+    session.pack()#outstanding element; placed last to be out of the way
 
     root.mainloop()
